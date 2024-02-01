ee737a2a897c2adf7e7623bee90feec230fd48ff
Felipe Aliaga <faliaga@siroe.cl>
2024-02-01 10:34:22 -0300

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'almacen_db' // Reemplaza con el nombre de tu base de datos
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 // Conecta a la base de datos
